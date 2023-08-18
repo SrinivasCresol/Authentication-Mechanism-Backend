32f881bdc6fd4c408c78fd9e2bc25e49d60d6b5d
Srinivas <srinivas.y@cresolinfoserv.com>
2023-08-18 09:51:44 +0530

    Updated Changes

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,38 +1,46 @@
 const users = require("../Models/AuthModel");
 const bcrypt = require("bcryptjs");
 
-//Register User
-
+// Register User
 exports.userRegister = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const registerUser = new users({ name, email, password: hashedPassword });
-    await registerUser.save();
-    return res.status(200).send("User Registered Successful");
+    const newUser = new users({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newUser.save();
+    res.status(200).send("User Registered Successfully");
   } catch (error) {
-    return res.status(500).send("Error Registering User");
+    console.error("Error Registering User:", error);
+    res.status(500).send("Error Registering User");
   }
 };
 
-//Login User
-
+// Login User
 exports.userLogin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const validUser = await users.findOne({ email: email });
+    const validUser = await users.findOne({ email });
     if (!validUser) {
       return res.status(404).send("User Not Found");
     }
+
     const isMatch = await bcrypt.compare(password, validUser.password);
     if (!isMatch) {
       return res.status(401).send("Incorrect Password");
     }
-    return res.status(200).send("Login Successful");
+
+    return res.status(200).send(validUser);
   } catch (error) {
-    return res.status(500).send("Login Failed");
+    console.error("Login Error:", error);
+    res.status(500).send("Login Failed");
   }
 };
